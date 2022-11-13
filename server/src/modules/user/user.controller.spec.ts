193c3fa55c706953b2aabec7f1d684f48ebb0579
Maid <zhp92@outlook.com>
2022-11-13 10:51:42 +0800

    test: add bcrypt test cases

--- server/src/modules/user/user.controller.spec.ts
+++ server/src/modules/user/user.controller.spec.ts
@@ -1,11 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { compareSync, hashSync } from 'bcrypt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 describe('UserController', () => {
   let userController: UserController;
   beforeEach(async () => {
     const user: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'maiiiiiid.fun',
+          port: 23306,
+          username: 'root',
+          password: '123456',
+          database: 'maid_comic_house',
+          entities: [User],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([User]),
+      ],
       controllers: [UserController],
       providers: [UserService],
     }).compile();
@@ -14,8 +30,10 @@ describe('UserController', () => {
   });
 
   describe('login', () => {
-    it('should return success', () => {
-      expect(userController.login({ name: '', password: '' })).toEqual({
+    it('test login use the admin account', async () => {
+      expect(
+        await userController.login({ name: 'admin', password: 'admin' }),
+      ).toEqual({
         state: 'success',
         data: null,
       });
@@ -23,11 +41,29 @@ describe('UserController', () => {
   });
 
   describe('regist', () => {
-    it('should return success', () => {
-      expect(userController.login({ name: '', password: '' })).toEqual({
-        state: 'success',
-        data: null,
+    it('test regist use an unregistered account', async () => {
+      const rep = await userController.regist({
+        name: 'test' + new Date().getTime(),
+        password: 'xxx',
       });
+      expect(rep.state).toEqual('success');
+    });
+  });
+
+  describe('bcrypt', () => {
+    it('test bcrypt', () => {
+      const pad = '4edfasdfssgsdgdhgd';
+      const hashPad = hashSync(pad, 6);
+      expect(compareSync(pad, hashPad)).toEqual(true);
+    });
+
+    it('test compareSync', () => {
+      expect(
+        compareSync(
+          'xxx',
+          '$2b$06$gBfOnai9OKRZ85WPi9IrdeJJrT1zhgHu0ebTHHcTZD2LysMdKr2cu',
+        ),
+      ).toEqual(true);
     });
   });
 });
