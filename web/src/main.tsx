c18fc9ecf5f89db7fdd2931b688d182c3a67837a
Maiiiiiid <zhp92@outlook.com>
2023-10-21 00:18:17 +0800

    update

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+// main.ts
+import 'virtual:uno.css'
 import './index.css'
 import App from './App'
 
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,10 +2,11 @@ import * as path from 'path'
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), UnoCSS()],
   resolve: {
     alias: {
       '/@': path.resolve(__dirname, './src'),
