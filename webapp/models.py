bac4853a550908a19f2de18e3c2d7aaec824c1d8
Maiiiiiid <zhp92@outlook.com>
2023-12-26 00:23:51 +0800

    update

--- webapp/models.py
+++ webapp/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 
 
-class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
-
-
 class Comic(models.Model):
     id = models.CharField(primary_key=True)
     url = models.CharField(max_length=1024)
@@ -24,7 +16,7 @@ class Comic(models.Model):
 
 class Chapter(models.Model):
     id = models.AutoField(primary_key=True)
-    comic = models.ForeignObjectRel(Comic, on_delete=models.CASCADE)
+    comic_id = models.ForeignObjectRel(Comic, on_delete=models.CASCADE)
     url = models.CharField(max_length=1024)
     name = models.CharField(max_length=100)
 
@@ -34,9 +26,45 @@ class Chapter(models.Model):
 
 class Image(models.Model):
     id = models.AutoField(primary_key=True)
-    chapter = models.ForeignObjectRel(Chapter, on_delete=models.CASCADE)
+    chapter_id = models.ForeignObjectRel(Chapter, on_delete=models.CASCADE)
     url = models.CharField(max_length=1024)
     path = models.CharField(max_length=1024)
 
     def __str__(self):
         return self.url
+
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class ComicAuthor(models.Model):
+    id = models.AutoField(primary_key=True)
+    comic_id = models.ForeignObjectRel(Comic, on_delete=models.CASCADE)
+    author_id = models.ForeignObjectRel(Author, on_delete=models.CASCADE)
+
+class ComicTag(models.Model):
+    id = models.AutoField(primary_key=True)
+    comic_id = models.ForeignObjectRel(Comic, on_delete=models.CASCADE)
+    Tag_id = models.ForeignObjectRel(Tag, on_delete=models.CASCADE)
